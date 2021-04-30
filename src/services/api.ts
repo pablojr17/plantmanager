c12efe2b693f20b3993745fb80ecb665e5602ff1
pablojr17 <pablojr17@gmail.com>
2021-04-29 22:44:42 -0300

    Deploy 🚲

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.108:3333'
+  baseURL: 'https://my-json-server.typicode.com/pablojr17/plantmanager'
 });
 
 export default api;
